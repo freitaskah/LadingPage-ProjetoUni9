ef6ba118ad731f8210ab90139fc07e6ccad544a2
freitaskah <143828862+freitaskah@users.noreply.github.com>
2024-05-30 13:43:36 -0300

    att
    att

--- Codigo Fonte/Codigo/Pizza Salgada/Assets/Js/MenuCategorias.js
+++ Codigo Fonte/Codigo/Pizza Salgada/Assets/Js/MenuCategorias.js
@@ -1,8 +1,12 @@
-var salgada = document.getElementById("salgada")
-var doce = document.getElementById("doce");
-var bebidas = document.getElementById("bebidas");
-var massas = document.getElementById("massas");
-var fogazza = document.getElementById("fogazza");
+var Beatriz = document.getElementById("Beatriz")
+var Gabriel_Eduardo = document.getElementById("Gabriel_Eduardo");
+var Jessica = document.getElementById("Jessica");
+var Julia = document.getElementById("Julia");
+var Kaiky = document.getElementById("Kaiky");
+var Gabriel_Viana = document.getElementById("Gabriel_Viana");
+var Maira = document.getElementById("Maira");
+var Leticia = document.getElementById("Leticia");
+var Mensagem = document.getElementById("Mensagem");
 
 function liga_display(elemento,estado){
     if(estado==1){
@@ -16,52 +20,77 @@ function liga_display(elemento,estado){
 
 function MudaMenu(x){
     if(x=="Beatriz"){
-        liga_display(salgada,0);
-        liga_display(doce,1);
-        liga_display(bebidas,1);
-        liga_display(massas,1);
-        liga_display(fogazza,1);
+        liga_display(Beatriz,0);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,1);
+        liga_display(Julia,1);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,1);
+        liga_display(Leticia,1);
     }else if(x=="Gabriel Eduardo"){
-        liga_display(salgada,1);
-        liga_display(doce,0);
-        liga_display(bebidas,1);
-        liga_display(massas,1);
-        liga_display(fogazza,1);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,0);
+        liga_display(Jessica,1);
+        liga_display(Julia,1);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,1);
+        liga_display(Leticia,1);
     }else if(x=="Jessica"){
-        liga_display(salgada,1);
-        liga_display(doce,1);
-        liga_display(bebidas,0);
-        liga_display(massas,1);
-        liga_display(fogazza,1);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,0);
+        liga_display(Julia,1);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,1);
+        liga_display(Leticia,1);
     }else if(x=="Julia"){
-        liga_display(salgada,1);
-        liga_display(doce,1);
-        liga_display(bebidas,1);
-        liga_display(massas,0);
-        liga_display(fogazza,1);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,1);
+        liga_display(Julia,0);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,1);
+        liga_display(Leticia,1);
     }else if(x=="Kaiky"){
-        liga_display(salgada,1);
-        liga_display(doce,1);
-        liga_display(bebidas,1);
-        liga_display(massas,1);
-        liga_display(fogazza,0);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,1);
+        liga_display(Julia,1);
+        liga_display(Kaiky,0);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,1);
+        liga_display(Leticia,1);
     }else if(x=="Gabriel Viana"){
-        liga_display(salgada,1);
-        liga_display(doce,1);
-        liga_display(bebidas,1);
-        liga_display(massas,1);
-        liga_display(fogazza,0);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,1);
+        liga_display(Julia,1);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,0);
+        liga_display(Maira,1);
+        liga_display(Leticia,1);
     }else if(x=="Maira"){
-        liga_display(salgada,1);
-        liga_display(doce,1);
-        liga_display(bebidas,1);
-        liga_display(massas,1);
-        liga_display(fogazza,0);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,1);
+        liga_display(Julia,1);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,0);
+        liga_display(Leticia,1);
     }else if(x=="Leticia"){
-        liga_display(salgada,1);
-        liga_display(doce,1);
-        liga_display(bebidas,1);
-        liga_display(massas,1);
-        liga_display(fogazza,0);
+        liga_display(Beatriz,1);
+        liga_display(Gabriel_Eduardo,1);
+        liga_display(Jessica,1);
+        liga_display(Julia,1);
+        liga_display(Kaiky,1);
+        liga_display(Gabriel_Viana,1);
+        liga_display(Maira,1);
+        liga_display(Leticia,0);
     } 
 }
+
